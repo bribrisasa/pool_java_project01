2236eac5215b71826e46cb3cff428fd7234b7912
sabrina <sabrina.savani@coding-academy.fr>
2017-08-18 14:40:26 +0200

    action listener

--- src/pool_java_project01/CONTROL/Controler.java
+++ src/pool_java_project01/CONTROL/Controler.java
@@ -1,10 +1,48 @@
 package pool_java_project01.CONTROL;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
 public class Controler {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
+		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		final DocumentBuilder builder = factory.newDocumentBuilder();
+		File fileXML = new File("src/pool_java_project01/CONTROL/conversionRate.xml");
+		Document xml = builder.parse(fileXML);
 		
+		final Element racine = xml.getDocumentElement();
+		final NodeList racineNoeuds = racine.getChildNodes();
+		final int nbRacineNoeuds = racineNoeuds.getLength();
 
+
+
+			for (int i = 0; i<nbRacineNoeuds; i++) {
+		        if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+		            final Element currency = (Element) racineNoeuds.item(i);
+					
+			   // System.out.println("Id : " + currency.getAttribute("id"));
+				
+			    final Element nameCurrency = (Element) currency.getElementsByTagName("name").item(0);
+			    final Element valueCurrency = (Element) currency.getElementsByTagName("Value").item(0);
+						
+			    if(nameCurrency.getTextContent().equalsIgnoreCase("Euro")) {
+			    	double d = Double.parseDouble(valueCurrency.getTextContent());
+			    		//System.out.println(d);
+			    }
+			    
+			   // System.out.println("Name : " + nameCurrency.getTextContent());
+			  //  System.out.println("Value : " + valueCurrency.getTextContent());
+		        }
+			}
+			
+			
 	}
-
 }
--- src/pool_java_project01/CONTROL/Converter.java
+++ src/pool_java_project01/CONTROL/Converter.java
@@ -1,7 +1,15 @@
 package pool_java_project01.CONTROL;
+import javax.swing.JFrame;
+
 import pool_java_project01.CONTROL.CURRENCIES.Currency;
+import pool_java_project01.GUI.WindowConverter;
 
 public class Converter {
+	WindowConverter view;
+	
+	public Converter(WindowConverter view) {
+		this.view = view;
+	}
 
 	public double toResult(Currency currency, double input){
 		double result;
@@ -13,5 +21,10 @@ public class Converter {
 	public String[] currenciesList() {
 		
 		return null;		
+	}
+	
+	public String convert(String amountOrigin, String currencyOrigin, String currencyTarget) {
+		System.out.println(amountOrigin + currencyOrigin + currencyTarget);
+		return (amountOrigin + currencyOrigin + currencyTarget);
 	}
 }
