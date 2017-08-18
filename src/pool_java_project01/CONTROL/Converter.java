54f99c28e1455f34607ba0e4f6435245a1e4f461
Laurent <Laurent@DESKTOP-D7G77LC>
2017-08-18 14:42:17 +0200

    converter update currenciesList

--- src/pool_java_project01/CONTROL/Converter.java
+++ src/pool_java_project01/CONTROL/Converter.java
@@ -1,4 +1,18 @@
 package pool_java_project01.CONTROL;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import pool_java_project01.CONTROL.CURRENCIES.Currency;
 
 public class Converter {
@@ -9,9 +23,25 @@ public class Converter {
 		result = currency.getConvertDollar()*input;
 		return result;	
 	}
-	
-	public String[] currenciesList() {
-		
-		return null;		
+
+	public ArrayList<String> currenciesList() throws SAXException, IOException, ParserConfigurationException {
+		ArrayList<String> currencies=new ArrayList<>();
+		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		final DocumentBuilder builder = factory.newDocumentBuilder();
+		File fileXML = new File("src/pool_java_project01/CONTROL/conversionRate.xml");
+
+		Document xml = builder.parse(fileXML);		
+		final Element racine = xml.getDocumentElement();
+		final NodeList racineNoeuds = racine.getChildNodes();
+		final int nbRacineNoeuds = racineNoeuds.getLength();
+
+		for (int i = 0; i<nbRacineNoeuds; i++) {
+			if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+				final Element currency = (Element) racineNoeuds.item(i);
+				final Element nameCurrency = (Element) currency.getElementsByTagName("name").item(0);			
+				currencies.add(nameCurrency.getTextContent());
+			}	
+		}
+		return currencies;
 	}
 }
