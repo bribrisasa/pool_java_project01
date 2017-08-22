7f0da380a4a7f77d9c60cb0e8edf1848cef1e9df
sabrina <sabrina.savani@coding-academy.fr>
2017-08-22 18:05:56 +0200

    fini

--- src/application/ControlerBloc.java
+++ src/application/ControlerBloc.java
@@ -2,15 +2,85 @@ package application;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputControl;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class ControlerBloc {
-	@FXML private AnchorPane Window;
-	
-	@FXML 
+	@FXML
+	AnchorPane Window;
+	@FXML
+	TextArea textAreaNote;
+	@FXML
+	Text okSave;
+
+	@FXML
 	private void processSuprBloc() {
 		Window.setVisible(false);
+	}
+
+	@FXML
+	private void saveNote() {
+		try {
+			FileWriter lu = new FileWriter("Note.txt");
+
+			BufferedWriter out = new BufferedWriter(lu);
+			System.out.println("");
+			out.write(textAreaNote.getText()); //
+			out.close();
+			okSave.setText("Saved.");
+
+		} catch (IOException er) {
+		}
+		
+	}
+
+	@FXML
+	private void openNote() {
+		try {
+			FileInputStream fis = new FileInputStream("Note.txt");
+			int n;
+
+			try {
+				while ((n = fis.available()) > 0) {
+					byte[] b = new byte[n];
+					int result = fis.read(b);
+					if (result == -1)
+						break;
+					String s = new String(b);
+					textAreaNote.setText(s);
+				}
+			} catch (IOException e) {
+				
+			}
+		} catch (FileNotFoundException err) {
+
+		}
+	}
+
+	public void chargeNote(Node text) {
+		String s="";
+		System.out.println(s);
+		try {
+			FileInputStream fis = new FileInputStream("Note.txt");
+			int n;
+
+				while ((n = fis.available()) > 0) {
+					byte[] b = new byte[n];
+					int result = fis.read(b);
+					if (result == -1)
+						break;
+					s = new String(b);
+					
+				}
+				((TextArea) text).setText(s);
+		}catch(Exception e) { e.printStackTrace();}
 	}
 }
--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,17 +1,22 @@
 package application;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -25,51 +30,56 @@ import javafx.stage.StageStyle;
 public class Controller {
 
 	private int changeCount;
-	private static String value="";
+	private static String value = "";
 	private double xOffset = 0;
 	private double yOffset = 0;
+	ControlerBloc cb;
 
-	@FXML private Label CalcLabel;
+	@FXML
+	private Label CalcLabel;
 
-	@FXML private TextField output;
+	@FXML
+	private TextField output;
 
-	@FXML private Pane Calc,ballance,Homebt,Plusdoc;
+	@FXML
+	private Pane Calc, ballance, Homebt, Plusdoc;
 
-	@FXML private AnchorPane Converter, Calculator,Home,Menu;
+	@FXML
+	private AnchorPane Converter, Calculator, Home, Menu;
+	
 
-	@FXML private FlowPane changePanel;
+	@FXML
+	private FlowPane changePanel;
 
-	@FXML 
-	private void processMenuButton(MouseEvent event){
-		if(event.getSource()== Calc){
+	@FXML
+	private void processMenuButton(MouseEvent event) {
+		if (event.getSource() == Calc) {
 			Home.setVisible(false);
 			Converter.setVisible(false);
 			Calculator.setVisible(true);
 			Calculator.toFront();
-		}
-		else if(event.getSource()== ballance){
+		} else if (event.getSource() == ballance) {
 			Home.setVisible(false);
 			Converter.setVisible(true);
 			Calculator.setVisible(false);
 			Converter.toFront();
-		}
-		else if(event.getSource()== Homebt){
+		} else if (event.getSource() == Homebt) {
 			Home.setVisible(true);
 			Converter.setVisible(false);
 			Calculator.setVisible(false);
 			Converter.toFront();
 		}
 	}
-	@FXML 
-	private void processExitButton(MouseEvent event){
+
+	@FXML
+	private void processExitButton(MouseEvent event) {
 		System.exit(0);
 	}
 
-	@FXML 
-	private void processAddButton() throws IOException{
+	@FXML
+	private void processAddButton() throws IOException {
 		Parent root;
-		if(changeCount<9)
-		{
+		if (changeCount < 9) {
 			root = FXMLLoader.load(getClass().getResource("ChangeWindow.fxml"));
 			AnchorPane scene = new AnchorPane(root);
 			changePanel.getChildren().add(new AnchorPane(scene));
@@ -77,37 +87,10 @@ public class Controller {
 		}
 	}
 
-	@FXML 
-	private void processNumpad(MouseEvent event){ 
-		Calculatrice calculette = new Calculatrice();
-		
-		final Pattern num = Pattern.compile("C=(\\d+\\.\\d+)");
-		final Pattern op = Pattern.compile("C=(\\+\\-\\*\\\\.\\,=)");
-		final Pattern cl = Pattern.compile("C=CLEAR");
-		
-		boolean isNum = num.matcher(value).matches();
-		boolean isOp = op.matcher(value).matches();
-		boolean isCl = cl.matcher(value).matches();
-		
-		System.out.println(value);
-		while(isNum) {
-			value=value.concat(((Button) event.getSource()).getText());
-			CalcLabel.setText(value);
-			isNum = num.matcher(value).matches();
-			isOp = op.matcher(value).matches();
-			isCl = cl.matcher(value).matches();
-		}
-		
-		System.out.println(isNum);
-		System.out.println(isOp);
-		System.out.println(isCl);
-		
-	}
-
-	@FXML 
-	private void processAddBloc() throws IOException{
+	@FXML
+	private void processAddBloc() throws IOException {
 		Parent root;
-		Stage BlocStage=new Stage();
+		Stage BlocStage = new Stage();
 		root = FXMLLoader.load(getClass().getResource("BlocNote.fxml"));
 		BlocStage.initStyle(StageStyle.TRANSPARENT);
 		root.setOnMousePressed(new EventHandler<MouseEvent>() {
@@ -128,9 +111,44 @@ public class Controller {
 		Scene scene = new Scene(root);
 		scene.setFill(Color.TRANSPARENT);
 		BlocStage.setScene(scene);
-		BlocStage.sizeToScene(); 
+		BlocStage.sizeToScene();
 		BlocStage.show();
 	}
-}
+
+	@FXML
+	private void openNote() throws IOException {
+		Parent root;
+		cb = new ControlerBloc();
+		Stage BlocStage=new Stage();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("BlocNote.fxml"));
+		root = loader.load();
+			
+			BlocStage.initStyle(StageStyle.TRANSPARENT);
+			root.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					xOffset = event.getSceneX();
+					yOffset = event.getSceneY();
+				}
+			});
+			root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					BlocStage.setX(event.getScreenX() - xOffset);
+					BlocStage.setY(event.getScreenY() - yOffset);
+				}
+			});
+			
+
+			Scene scene = new Scene(root);
+			scene.setFill(Color.TRANSPARENT);
+			BlocStage.setScene(scene);
+			BlocStage.sizeToScene(); 
+			cb.chargeNote((Node) loader.getNamespace().get("textAreaNote"));
+			BlocStage.show();
+			
+					
+					}
+				} 
 
 
